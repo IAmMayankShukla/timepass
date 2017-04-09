546ec6f3451fcb3d99e7d78aed3a74f734fde9cf
Mayank Shukla <munksiitm10@gmail.com>
2017-04-09 17:25:52 +0530

    macro test modified
    Signed-off-by: Mayank Shukla <munksiitm10@gmail.com>

--- C/macroQuestions.c
+++ C/macroQuestions.c
@@ -28,6 +28,37 @@
     #define B 3
 #endif
 
+#define INFO    1
+#define ERR 2
+#define STD_OUT stdout
+#define STD_ERR stderr
+ 
+#define LOG_MESSAGE(prio, stream, msg, ...) do {\
+                        char *str;\
+                        if (prio == INFO)\
+                            str = "INFO";\
+                        else if (prio == ERR)\
+                            str = "ERR";\
+                        fprintf(stream, "[%s] : %s : %d : "msg" \n", \
+                                str, __FILE__, __LINE__, ##__VA_ARGS__);\
+                    } while (0)
+
+#define CRASH() do { \
+      ((void(*)())0)(); \
+   } while(0)
+
+#define OFFSET_OF(TYPE, element) ((size_t)&(((TYPE *)0)->element))
+
+#define likely(x)      __builtin_expect(!!(x), 1)
+#define unlikely(x)    __builtin_expect(!!(x), 0)
+
+typedef struct PodTag
+{
+   int     i;
+   double  d;
+   char    c;
+} PodType;
+
 void marcoQ1(void)
 {
 	int t = 0;
@@ -52,6 +83,40 @@ void macroQ4(void)
 	printf("Value Of Max %d\n", MAX);
 	printf("Size Of Max %d\n", sizeof(MAX));
 	printf("Size Of Nax %d\n", sizeof(NAX));
+}
+
+void macroQ5(void)
+{
+    char *s = "Hello";
+ 
+        /* display normal message */
+    LOG_MESSAGE(ERR, STD_ERR, "Failed to open file");
+ 
+    /* provide string as argument */
+    LOG_MESSAGE(INFO, STD_OUT, "%s Geeks for Geeks", s);
+ 
+    /* provide integer as arguments */
+    LOG_MESSAGE(INFO, STD_OUT, "%d + %d = %d", 10, 20, (10 + 20));
+}
+
+void macroQ6(void)
+{
+    CRASH();
+}
+
+void macroQ7(void)
+{
+    printf("offset of c in structure is %d\n", OFFSET_OF(PodType, c) );
+}
+
+void macroQ8(void)
+{
+    const char *home_dir ;
+    home_dir = getenv("HOME");
+    if (likely(home_dir)) 
+        printf("home directory: %s\n", home_dir);
+    else
+        perror("getenv");
 }
 
 int main(unsigned int argc, char *argv[])
@@ -91,12 +156,41 @@ int main(unsigned int argc, char *argv[])
 		}
 		break;
 
+        case 5:
+        {
+            printf("Macro Question 5 selected\n");
+            macroQ5();
+        }
+        break;
+
+        case 6:
+        {
+            printf("Macro Question 6 selected\n");
+            macroQ6();
+        }
+        break;
+
+        case 7:
+        {
+            printf("Macro Question 7 Selected\n");
+            macroQ7();
+        }
+        break;
+
+        case 8:
+        {
+            printf("Macro Question 8 selected\n");
+            macroQ8();
+        }
+        break;
+
 		default:
 		{
 			printf("Nothing is selected\n");
 			iRet = -1;
 		}
 		break;
+
 	}
 
 	return iRet;
